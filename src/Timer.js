9260502c8f1a3a48345589d6da320b92fc22fa21
azotamiota <azotamiota@gmail.com>
2022-02-14 10:44:27 +0000

    mobile version ready

--- src/Timer.js
+++ src/Timer.js
@@ -66,7 +66,7 @@ function Timer() {
     
   function timerToggle() {
       if (isCountdownActive === false) {
-        ticker = setInterval(secondDecreaser, 50);
+        ticker = setInterval(secondDecreaser, 1000);
         setCountdownActivity(true);
       } else {
         clearInterval(ticker);
@@ -116,87 +116,103 @@ function Timer() {
 
   return (
     <>
-      <div className="container mt-5 mb-5 ms-auto me-auto p-2">
-          <div id="break-label" className="row justify-content-center p-2">
-            Break length
-          </div>
-          <div className="row justify-content-evenly">
-            <button
-              type="button"
-              className="btn btn-secondary col-2"
-              id="break-decrement"
-              onClick={() => breakDecrement()}
-            >
-              <i className="fas fa-chevron-down"></i>
-            </button>
-            <div id="break-length" className="col-2 text-center">
-              5
-            </div>
-            <button
-              type="button"
-              className="btn btn-secondary col-2"
-              id="break-increment"
-              onClick={() => breakIncrement()}
-            >
-              <i className="fas fa-chevron-up"></i>
-            </button>
-          </div>
-          <div className="row justify-content-evenly mt-4">
-            <div id="session-label" className="row justify-content-center p-2">
-              Session length
-            </div>
-            <button
-              type="button"
-              className="btn btn-secondary col-2"
-              id="session-decrement"
-              onClick={() => sessionDecrement()}
-            >
-              <i className="fas fa-chevron-down"></i>
-            </button>
-            <div id="session-length" className="col-2 text-center">
-              {defaultSessionLength}
-            </div>
-            <button
-              type="button"
-              className="btn btn-secondary col-2"
-              id="session-increment"
-              onClick={() => sessionIncrement()}
-            >
-              <i className="fas fa-chevron-up"></i>
-            </button>
-          </div>
-          <section
-            id="session-timer-panel"
-            className="row m-3 p-3 justify-content-center text-center"
-          >
-            <div id="timer-label">{panelLabel}</div>
-            <div id="time-left">{clockStyle()}</div>
-          </section>
-          <div id="navigation" className="row justify-content-center">
-            <button
-              type="button"
-              className="btn btn-secondary col-3"
-              id="start_stop"
-              onClick={timerToggle}
-            >
-              <i className="fas fa-play"></i>
-              <i className="fas fa-pause"></i>
-            </button>
-            <button
-              type="button"
-              className="btn btn-secondary col-3"
-              id="reset"
-              onClick={reset}
-            >
-              <i className="fas fa-repeat"></i>
-            </button>
-          </div>
-          <audio
-            id="beep"
-            src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
-          />
+      <div className="container mt-3 mb-5 ms-auto me-auto p-2">
+        <div
+          id="break-label"
+          className="row timer-text justify-content-center p-2"
+        >
+          Break length
         </div>
-      
+        <div className="row justify-content-evenly">
+          <button
+            type="button"
+            className="btn btn-secondary col-2"
+            id="break-decrement"
+            onClick={() => breakDecrement()}
+          >
+            <i className="fas fa-chevron-down"></i>
+          </button>
+          <div
+            id="break-length"
+            className="bg-light col-2 timer-text d-flex justify-content-center align-items-center"
+          >
+            5
+          </div>
+          <button
+            type="button"
+            className="btn btn-secondary col-2"
+            id="break-increment"
+            onClick={() => breakIncrement()}
+          >
+            <i className="fas fa-chevron-up"></i>
+          </button>
+        </div>
+        <div className="row justify-content-evenly mt-4">
+          <div
+            id="session-label"
+            className="row timer-text justify-content-center p-2"
+          >
+            Session length
+          </div>
+          <button
+            type="button"
+            className="btn btn-secondary col-2"
+            id="session-decrement"
+            onClick={() => sessionDecrement()}
+          >
+            <i className="fas fa-chevron-down"></i>
+          </button>
+          <div
+            id="session-length"
+            className="bg-light col-2 timer-text d-flex justify-content-center align-items-center"
+          >
+            {defaultSessionLength}
+          </div>
+          <button
+            type="button"
+            className="btn btn-secondary col-2"
+            id="session-increment"
+            onClick={() => sessionIncrement()}
+          >
+            <i className="fas fa-chevron-up"></i>
+          </button>
+        </div>
+        <section
+          id="session-timer-panel"
+          className="row timer-text mt-3 mb-3 ms-auto me-auto p-3 justify-content-center text-center"
+        >
+          <div id="timer-label">{panelLabel}</div>
+          <div id="time-left">{clockStyle()}</div>
+        </section>
+        <div id="navigation" className="row justify-content-center">
+          <button
+            type="button"
+            className="btn btn-secondary col-3 m-1"
+            id="start_stop"
+            onClick={timerToggle}
+          >
+            <i className="fas fa-play"></i>
+            <i className="fas fa-pause"></i>
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary col-3 m-1"
+            id="reset"
+            onClick={reset}
+          >
+            <i className="fas fa-repeat"></i>
+          </button>
+        </div>
+        <audio
+          id="beep"
+          src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+        />
+      </div>
+      <footer id="copyright" className='m-3'>
+        Designed and Coded By
+        <br />
+        <a href="https://azotamiota.github.io">azotamiota</a>
+      </footer>
     </>
   );
 }
