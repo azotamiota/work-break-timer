a3ab7262bed21a2aeecebe75407a8bb9275f3ccb
azotamiota <azotamiota@gmail.com>
2022-02-14 01:11:47 +0000

    styling in process

--- src/Timer.js
+++ src/Timer.js
@@ -116,35 +116,87 @@ function Timer() {
 
   return (
     <>
-      <div id="break-label">
-        Break label: <span id="break-length">5</span>
-      </div>
-      <div id="session-label">
-        Session length: <span id="session-length">{defaultSessionLength}</span>
-      </div>
-      <button id="break-decrement" onClick={() => breakDecrement()}>
-        Decrease break time
-      </button>
-      <button id="session-decrement" onClick={() => sessionDecrement()}>
-        Decrease session time
-      </button>
-      <button id="break-increment" onClick={() => breakIncrement()}>
-        Increase break time
-      </button>
-      <button id="session-increment" onClick={() => sessionIncrement()}>
-        Increase session time
-      </button>
-      <section id="session-timer-panel">
-        <div id="timer-label">{panelLabel}</div>
-        <div id="time-left">{clockStyle()}</div>
-      </section>
-      <button id="start_stop" onClick={timerToggle}>
-        Start / Pause
-      </button>
-      <button id="reset" onClick={reset}>
-        Reset
-      </button>
-      <audio id='beep' src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav" />
+      <div className="container mt-5 mb-5 ms-auto me-auto p-2">
+          <div id="break-label" className="row justify-content-center p-2">
+            Break length
+          </div>
+          <div className="row justify-content-evenly">
+            <button
+              type="button"
+              className="btn btn-secondary col-2"
+              id="break-decrement"
+              onClick={() => breakDecrement()}
+            >
+              <i className="fas fa-chevron-down"></i>
+            </button>
+            <div id="break-length" className="col-2 text-center">
+              5
+            </div>
+            <button
+              type="button"
+              className="btn btn-secondary col-2"
+              id="break-increment"
+              onClick={() => breakIncrement()}
+            >
+              <i className="fas fa-chevron-up"></i>
+            </button>
+          </div>
+          <div className="row justify-content-evenly mt-4">
+            <div id="session-label" className="row justify-content-center p-2">
+              Session length
+            </div>
+            <button
+              type="button"
+              className="btn btn-secondary col-2"
+              id="session-decrement"
+              onClick={() => sessionDecrement()}
+            >
+              <i className="fas fa-chevron-down"></i>
+            </button>
+            <div id="session-length" className="col-2 text-center">
+              {defaultSessionLength}
+            </div>
+            <button
+              type="button"
+              className="btn btn-secondary col-2"
+              id="session-increment"
+              onClick={() => sessionIncrement()}
+            >
+              <i className="fas fa-chevron-up"></i>
+            </button>
+          </div>
+          <section
+            id="session-timer-panel"
+            className="row m-3 p-3 justify-content-center text-center"
+          >
+            <div id="timer-label">{panelLabel}</div>
+            <div id="time-left">{clockStyle()}</div>
+          </section>
+          <div id="navigation" className="row justify-content-center">
+            <button
+              type="button"
+              className="btn btn-secondary col-3"
+              id="start_stop"
+              onClick={timerToggle}
+            >
+              <i className="fas fa-play"></i>
+              <i className="fas fa-pause"></i>
+            </button>
+            <button
+              type="button"
+              className="btn btn-secondary col-3"
+              id="reset"
+              onClick={reset}
+            >
+              <i className="fas fa-repeat"></i>
+            </button>
+          </div>
+          <audio
+            id="beep"
+            src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+          />
+        </div>
+      
     </>
   );
 }
