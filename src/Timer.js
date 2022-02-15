1fa729a230950050a935a771aaaad00962680d80
azotamiota <azotamiota@gmail.com>
2022-02-15 16:00:55 +0000

    bug fixes

--- src/Timer.js
+++ src/Timer.js
@@ -13,9 +13,9 @@ function Timer() {
   const [isSessionRound, setSessionRound] = useState(true)
   const [isCountdownActive, setCountdownActivity] = useState(false);
   const [leftSeconds, setLeftSeconds] = useState(sessionSeconds);
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState(0); //this is for forcing render only
 
-  let clockStyle = () => {
+  const ClockStyle = () => {
       let displayMinutes = Math.floor(leftSeconds / 60);
       let displaySeconds = leftSeconds - displayMinutes * 60;
       displayMinutes =
@@ -33,8 +33,6 @@ function Timer() {
       if (isCountdownActive === false) {
         if (sessionSeconds < 3600) {
           defaultSessionLength.current += 1;
-          console.log('defaultSessionLength: ', defaultSessionLength.current)
-          console.log('sessionSeconds should increment here: ', sessionSeconds)
           setLeftSeconds(sessionSeconds + 60);
         }
         }
@@ -72,7 +70,7 @@ function Timer() {
     
   const timerToggle = () => {
       if (isCountdownActive === false) {
-        ticker = setInterval(secondDecreaser, 1000);
+        ticker = setInterval(secondDecreaser, 10);
         setCountdownActivity(true);
       } else {
         clearInterval(ticker);
@@ -116,7 +114,6 @@ function Timer() {
     setCountdownActivity(false)
     setLeftSeconds(defaultSessionLength.current * 60);
     setSessionRound(true);
-   document.getElementById("break-length").innerHTML = 5;
     
   }
 
@@ -188,7 +185,7 @@ function Timer() {
           className="row timer-text mt-3 mb-3 ms-auto me-auto p-3 justify-content-center text-center"
         >
           <div id="timer-label">{panelLabel}</div>
-          <div id="time-left">{clockStyle()}</div>
+          <div id="time-left">{<ClockStyle />}</div>
         </section>
         <div id="navigation" className="row justify-content-center">
           <button
